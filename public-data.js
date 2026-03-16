7f587212baab2e5d07846e43a58f9ebe1f21b53f
vivekjai2000 <vivekjai2000@gmail.com>
2026-03-15 23:32:59 -0400

    Add files via upload

--- public-data.js
+++ public-data.js
@@ -8,12 +8,766 @@ window.POUNCY_PUBLIC_DATA = {
     "defaultRounds": 8,
     "winScore": 11,
     "kFactor": 24,
-    "minMatchesForPlayoffs": 8,
-    "playoffFormat": 12
+    "minMatchesForPlayoffs": 21,
+    "playoffFormat": 8
   },
-  "roster": [],
-  "weeks": [],
-  "availability": {},
+  "roster": [
+    {
+      "name": "Sheel",
+      "rating": 4.25,
+      "seed": 1,
+      "elo": 416.5667496369882,
+      "joinedWeek": 1,
+      "active": true
+    },
+    {
+      "name": "Vivek",
+      "rating": 4.02,
+      "seed": 2,
+      "elo": 405.6735526680722,
+      "joinedWeek": 1,
+      "active": true
+    },
+    {
+      "name": "Murali",
+      "rating": 4,
+      "seed": 3,
+      "elo": 423.7033605052487,
+      "joinedWeek": 1,
+      "active": true
+    },
+    {
+      "name": "Ravi",
+      "rating": 4,
+      "seed": 4,
+      "elo": 376.2966394947513,
+      "joinedWeek": 1,
+      "active": true
+    },
+    {
+      "name": "Sunil",
+      "rating": 4,
+      "seed": 5,
+      "elo": 396.32644733192774,
+      "joinedWeek": 1,
+      "active": true
+    },
+    {
+      "name": "Vimal",
+      "rating": 4,
+      "seed": 6,
+      "elo": 379.80599415184236,
+      "joinedWeek": 1,
+      "active": true
+    },
+    {
+      "name": "Vin",
+      "rating": 4,
+      "seed": 7,
+      "elo": 420.19400584815764,
+      "joinedWeek": 1,
+      "active": true
+    },
+    {
+      "name": "Doumit",
+      "rating": 3.75,
+      "seed": 8,
+      "elo": 383.4332503630118,
+      "joinedWeek": 1,
+      "active": true
+    }
+  ],
+  "weeks": [
+    {
+      "week": 1,
+      "saturday": {
+        "date": "2026-04-11",
+        "active": true,
+        "courts": 2,
+        "rounds": 2,
+        "schedule": [
+          {
+            "id": "R1C1",
+            "round": 1,
+            "court": 1,
+            "team1": [
+              "Doumit",
+              "Murali"
+            ],
+            "team2": [
+              "Ravi",
+              "Sheel"
+            ],
+            "score1": 11,
+            "score2": 2,
+            "completed": true
+          },
+          {
+            "id": "R1C2",
+            "round": 1,
+            "court": 2,
+            "team1": [
+              "Sunil",
+              "Vimal"
+            ],
+            "team2": [
+              "Vin",
+              "Vivek"
+            ],
+            "score1": 11,
+            "score2": 8,
+            "completed": true
+          },
+          {
+            "id": "R2C1",
+            "round": 2,
+            "court": 1,
+            "team1": [
+              "Doumit",
+              "Ravi"
+            ],
+            "team2": [
+              "Murali",
+              "Sheel"
+            ],
+            "score1": 2,
+            "score2": 11,
+            "completed": true
+          },
+          {
+            "id": "R2C2",
+            "round": 2,
+            "court": 2,
+            "team1": [
+              "Sunil",
+              "Vin"
+            ],
+            "team2": [
+              "Vimal",
+              "Vivek"
+            ],
+            "score1": 11,
+            "score2": 4,
+            "completed": true
+          }
+        ]
+      },
+      "sunday": {
+        "date": "2026-04-12",
+        "active": true,
+        "courts": 2,
+        "rounds": 2,
+        "schedule": [
+          {
+            "id": "R1C1",
+            "round": 1,
+            "court": 1,
+            "team1": [
+              "Doumit",
+              "Murali"
+            ],
+            "team2": [
+              "Ravi",
+              "Sheel"
+            ],
+            "score1": 11,
+            "score2": 7,
+            "completed": true
+          },
+          {
+            "id": "R1C2",
+            "round": 1,
+            "court": 2,
+            "team1": [
+              "Sunil",
+              "Vimal"
+            ],
+            "team2": [
+              "Vin",
+              "Vivek"
+            ],
+            "score1": 1,
+            "score2": 11,
+            "completed": true
+          },
+          {
+            "id": "R2C1",
+            "round": 2,
+            "court": 1,
+            "team1": [
+              "Doumit",
+              "Ravi"
+            ],
+            "team2": [
+              "Murali",
+              "Sheel"
+            ],
+            "score1": 13,
+            "score2": 11,
+            "completed": true
+          },
+          {
+            "id": "R2C2",
+            "round": 2,
+            "court": 2,
+            "team1": [
+              "Sunil",
+              "Vin"
+            ],
+            "team2": [
+              "Vimal",
+              "Vivek"
+            ],
+            "score1": 15,
+            "score2": 13,
+            "completed": true
+          }
+        ]
+      }
+    },
+    {
+      "week": 2,
+      "saturday": {
+        "date": "2026-04-18",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      },
+      "sunday": {
+        "date": "2026-04-19",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      }
+    },
+    {
+      "week": 3,
+      "saturday": {
+        "date": "2026-04-25",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      },
+      "sunday": {
+        "date": "2026-04-26",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      }
+    },
+    {
+      "week": 4,
+      "saturday": {
+        "date": "2026-05-02",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      },
+      "sunday": {
+        "date": "2026-05-03",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      }
+    },
+    {
+      "week": 5,
+      "saturday": {
+        "date": "2026-05-09",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      },
+      "sunday": {
+        "date": "2026-05-10",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      }
+    },
+    {
+      "week": 6,
+      "saturday": {
+        "date": "2026-05-16",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      },
+      "sunday": {
+        "date": "2026-05-17",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      }
+    },
+    {
+      "week": 7,
+      "saturday": {
+        "date": "2026-05-23",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      },
+      "sunday": {
+        "date": "2026-05-24",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      }
+    },
+    {
+      "week": 8,
+      "saturday": {
+        "date": "2026-05-30",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      },
+      "sunday": {
+        "date": "2026-05-31",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      }
+    },
+    {
+      "week": 9,
+      "saturday": {
+        "date": "2026-06-06",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      },
+      "sunday": {
+        "date": "2026-06-07",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      }
+    },
+    {
+      "week": 10,
+      "saturday": {
+        "date": "2026-06-13",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      },
+      "sunday": {
+        "date": "2026-06-14",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      }
+    },
+    {
+      "week": 11,
+      "saturday": {
+        "date": "2026-06-20",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      },
+      "sunday": {
+        "date": "2026-06-21",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      }
+    },
+    {
+      "week": 12,
+      "saturday": {
+        "date": "2026-06-27",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      },
+      "sunday": {
+        "date": "2026-06-28",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      }
+    },
+    {
+      "week": 13,
+      "saturday": {
+        "date": "2026-07-04",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      },
+      "sunday": {
+        "date": "2026-07-05",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      }
+    },
+    {
+      "week": 14,
+      "saturday": {
+        "date": "2026-07-11",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      },
+      "sunday": {
+        "date": "2026-07-12",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      }
+    },
+    {
+      "week": 15,
+      "saturday": {
+        "date": "2026-07-18",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      },
+      "sunday": {
+        "date": "2026-07-19",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      }
+    },
+    {
+      "week": 16,
+      "saturday": {
+        "date": "2026-07-25",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      },
+      "sunday": {
+        "date": "2026-07-26",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      }
+    },
+    {
+      "week": 17,
+      "saturday": {
+        "date": "2026-08-01",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      },
+      "sunday": {
+        "date": "2026-08-02",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      }
+    },
+    {
+      "week": 18,
+      "saturday": {
+        "date": "2026-08-08",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      },
+      "sunday": {
+        "date": "2026-08-09",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      }
+    },
+    {
+      "week": 19,
+      "saturday": {
+        "date": "2026-08-15",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      },
+      "sunday": {
+        "date": "2026-08-16",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      }
+    },
+    {
+      "week": 20,
+      "saturday": {
+        "date": "2026-08-22",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      },
+      "sunday": {
+        "date": "2026-08-23",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      }
+    },
+    {
+      "week": 21,
+      "saturday": {
+        "date": "2026-08-29",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      },
+      "sunday": {
+        "date": "2026-08-30",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      }
+    },
+    {
+      "week": 22,
+      "saturday": {
+        "date": "2026-09-05",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      },
+      "sunday": {
+        "date": "2026-09-06",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      }
+    },
+    {
+      "week": 23,
+      "saturday": {
+        "date": "2026-09-12",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      },
+      "sunday": {
+        "date": "2026-09-13",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      }
+    },
+    {
+      "week": 24,
+      "saturday": {
+        "date": "2026-09-19",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      },
+      "sunday": {
+        "date": "2026-09-20",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      }
+    },
+    {
+      "week": 25,
+      "saturday": {
+        "date": "2026-09-26",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      },
+      "sunday": {
+        "date": "2026-09-27",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      }
+    },
+    {
+      "week": 26,
+      "saturday": {
+        "date": "2026-10-03",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      },
+      "sunday": {
+        "date": "2026-10-04",
+        "active": false,
+        "courts": 5,
+        "rounds": 8,
+        "schedule": []
+      }
+    }
+  ],
+  "availability": {
+    "W1_saturday": [
+      "Sheel",
+      "Vivek",
+      "Murali",
+      "Ravi",
+      "Sunil",
+      "Vimal",
+      "Vin",
+      "Doumit"
+    ]
+  },
   "playoffs": {},
-  "season": []
+  "season": [
+    {
+      "name": "Murali",
+      "wins": 3,
+      "losses": 1,
+      "pf": 44,
+      "pa": 24,
+      "elo": 423.7033605052487,
+      "active": true,
+      "games": 4,
+      "pct": 0.75,
+      "diff": 20,
+      "playoffEligible": false
+    },
+    {
+      "name": "Vin",
+      "wins": 3,
+      "losses": 1,
+      "pf": 45,
+      "pa": 29,
+      "elo": 420.19400584815764,
+      "active": true,
+      "games": 4,
+      "pct": 0.75,
+      "diff": 16,
+      "playoffEligible": false
+    },
+    {
+      "name": "Doumit",
+      "wins": 3,
+      "losses": 1,
+      "pf": 37,
+      "pa": 31,
+      "elo": 383.4332503630118,
+      "active": true,
+      "games": 4,
+      "pct": 0.75,
+      "diff": 6,
+      "playoffEligible": false
+    },
+    {
+      "name": "Sunil",
+      "wins": 3,
+      "losses": 1,
+      "pf": 38,
+      "pa": 36,
+      "elo": 396.32644733192774,
+      "active": true,
+      "games": 4,
+      "pct": 0.75,
+      "diff": 2,
+      "playoffEligible": false
+    },
+    {
+      "name": "Vivek",
+      "wins": 1,
+      "losses": 3,
+      "pf": 36,
+      "pa": 38,
+      "elo": 405.6735526680722,
+      "active": true,
+      "games": 4,
+      "pct": 0.25,
+      "diff": -2,
+      "playoffEligible": false
+    },
+    {
+      "name": "Sheel",
+      "wins": 1,
+      "losses": 3,
+      "pf": 31,
+      "pa": 37,
+      "elo": 416.5667496369882,
+      "active": true,
+      "games": 4,
+      "pct": 0.25,
+      "diff": -6,
+      "playoffEligible": false
+    },
+    {
+      "name": "Vimal",
+      "wins": 1,
+      "losses": 3,
+      "pf": 29,
+      "pa": 45,
+      "elo": 379.80599415184236,
+      "active": true,
+      "games": 4,
+      "pct": 0.25,
+      "diff": -16,
+      "playoffEligible": false
+    },
+    {
+      "name": "Ravi",
+      "wins": 1,
+      "losses": 3,
+      "pf": 24,
+      "pa": 44,
+      "elo": 376.2966394947513,
+      "active": true,
+      "games": 4,
+      "pct": 0.25,
+      "diff": -20,
+      "playoffEligible": false
+    }
+  ]
 };
