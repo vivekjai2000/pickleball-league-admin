80106e45e7e72b6a7401afa9384441ccf79d1401
vivekjai2000 <vivekjai2000@gmail.com>
2026-03-18 13:41:31 -0400

    Add files via upload

---
+++ public-data_patched.js
@@ -0,0 +1,20 @@
+// IMPORTANT: Replace the googleScriptUrl value below with your deployed Apps Script Web App URL.
+window.POUNCEY_PUBLIC_DATA = {
+  "profile": {
+    "leagueTitle": "Pouncey Tract Weekend Pickleball League",
+    "leagueSubtitle": "Short Pump, VA",
+    "seasonStart": "2026-04-11",
+    "seasonEnd": "2026-10-03",
+    "defaultCourts": 5,
+    "defaultRounds": 8,
+    "winScore": 11,
+    "kFactor": 24,
+    "minMatchesForPlayoffs": 8,
+    "googleScriptUrl": "PASTE_YOUR_DEPLOYED_GOOGLE_APPS_SCRIPT_URL_HERE"
+  },
+  "roster": [],
+  "weeks": [],
+  "availability": {},
+  "playoffs": {},
+  "season": []
+};
